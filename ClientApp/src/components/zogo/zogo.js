8eb1a6d250618955c6e721a766d879f379d71be5
twtseng <twtseng@users.noreply.github.com>
2020-10-28 15:01:12 -0700

    Zogo lava dodge components

--- ClientApp/src/components/zogo/zogo.js
+++ ClientApp/src/components/zogo/zogo.js
@@ -7,68 +7,68 @@ import zogo_1_png from '../../static/zogo_1.png';
 import zogo_dodge_png from '../../static/zogodogetrall.png';
 import './zogo.css';
 
-const StartScreen = (setLevel) => {
+const StartScreen = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
             <img src={zogo_1_png} style={{width:"80%"}}/>
-            <Button className="startbutton" onClick={() => setLevel(1)}></Button>
+            <Button className="startbutton" onClick={() => props.setScreen(1)}></Button>
         </Jumbotron>    
     )
 }
-const PickLevelScreen = (setLevel) => {
+const PickLevelScreen = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
             <h1>Pick Level Screen</h1>
-            <Button className="startbutton" onClick={() => setLevel(2)}></Button>
+            <Button className="startbutton" onClick={() => props.setScreen(2)}></Button>
         </Jumbotron>    
     )
 }
-const PickCharacterScreen = (setLevel) => {
+const PickCharacterScreen = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
             <h1>Pick Character Screen</h1>
-            <Button className="startbutton" onClick={() => setLevel(3)}></Button>
+            <Button className="startbutton" onClick={() => props.setScreen(3)}></Button>
         </Jumbotron>    
     )
 }
-const GetReadyToDodge = (setLevel) => {
+const GetReadyToDodge = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
             <h1>Get ready to dodge</h1>
-            <Button className="startbutton" onClick={() => setLevel(4)}></Button>
+            <Button className="startbutton" onClick={() => props.setScreen(4)}></Button>
         </Jumbotron>    
     )
 }
-const LavaDodge = (setLevel) => {
+const LavaDodge = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
             <h1>Start Dodging!!!</h1>
             <img src={zogo_dodge_png} style={{width:"80%"}}/>
-            <Button className="startbutton" onClick={() => setLevel(5)}></Button>
+            <Button className="startbutton" onClick={() => props.setScreen(5)}></Button>
         </Jumbotron>    
     )
 }
-const YouWin = (setLevel) => {
+const YouWin = (props) => {
     return (
         <Jumbotron className="gameJumbotron">
-            <h1>You Win</h1>
-            <Button className="startbutton" onClick={() => setLevel(0)}></Button>
+            <h1>You Win my goooooood friend</h1>
+            <Button className="startbutton" onClick={() => props.setScreen(0)}></Button>
         </Jumbotron>    
     )
 }
 const Game = () => {
 
-    const [level, setLevel] = React.useState(0);
+    const [screen, setScreen] = React.useState(0);
     const screens = [
-        StartScreen, 
-        PickLevelScreen, 
-        PickCharacterScreen, 
-        GetReadyToDodge,
-        LavaDodge,
-        YouWin
+        <StartScreen setScreen={setScreen} />, 
+        <PickLevelScreen setScreen={setScreen} />, 
+        <PickCharacterScreen setScreen={setScreen} />, 
+        <GetReadyToDodge setScreen={setScreen} />,
+        <LavaDodge setScreen={setScreen} />,
+        <YouWin setScreen={setScreen} />
     ];
     return (
-        screens[level](setLevel)
+        screens[screen]
     )
 }
 
